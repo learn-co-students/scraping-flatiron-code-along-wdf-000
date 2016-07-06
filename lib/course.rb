d094dcc9fd76f18b9ce40a7f232e358f5756a674
aehmt <calisahmet@gmail.com>
2016-07-06 12:51:25 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,29 @@
+require 'nokogiri'
+require 'open-uri'
 
+class Course
+  attr_accessor :title, :schedule
+  attr_writer :description
+  @@courses = []
 
+  def initialize
+    save
+  end
+
+  def save
+    @@courses << self
+  end
+
+  def description
+    "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover"
+  end
+
+  def self.reset_all
+    @@courses.clear
+  end
+
+  def self.all
+    @@courses
+  end
+    
+end
